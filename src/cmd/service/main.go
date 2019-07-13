95b0f0b27c7a3a3ce8741b0a75fa055f8fd3397b
Samuel <samuell.person@gmail.com>
2019-07-13 17:49:48 +0200

    add: partial validation

--- src/cmd/service/main.go
+++ src/cmd/service/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -15,14 +16,52 @@ type Sms struct {
 	CreatedBy string    `json:"created_by"`
 }
 
+type ConfirmationCodeRequest struct {
+	ConfirmationCode string `json:"confirmation_code"`
+	PhoneNumber      string `json:"phone_number"`
+}
+
+func (request *ConfirmationCodeRequest) validate() url.Values {
+	err := url.Values{}
+
+	if request.ConfirmationCode == "" {
+		err.Add("confirmation_code", "This field is required")
+	}
+
+	if request.PhoneNumber == "" {
+		err.Add("phone_number", "This field is required")
+	}
+
+	// validate phone number
+
+	return err
+}
+
 func SendConfirmationCodeEndpoint(w http.ResponseWriter, r *http.Request) {
+	requestBody := &ConfirmationCodeRequest{}
+
+	defer r.Body.Close()
+	if err := json.NewDecoder(r.Body).Decode(requestBody); err != nil {
+		panic(err)
+	}
+
+	if errors := requestBody.validate(); len(errors) > 0 {
+		err := map[string]interface{}{"error": errors}
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err)
+	}
+
 	sms := Sms{
 		ID:        "1",
-		To:        "+46701234567",
-		Content:   "Confirmation code: 0ldi2m",
+		To:        requestBody.PhoneNumber,
+		Content:   requestBody.ConfirmationCode,
 		CreatedAt: time.Now(),
 		CreatedBy: "API KEY",
 	}
+	// save instance
+	// call Twilio API
+
 	json.NewEncoder(w).Encode(sms)
 }
 
