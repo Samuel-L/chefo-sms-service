76a15db1caa72663ab909abe694a86a00c5c2bfb
Samuel <samuell.person@gmail.com>
2019-07-13 18:48:37 +0200

    refactor: project structure

---
+++ cmd/service/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/samuel-l/chefo-sms-service/service"
+)
+
+func main() {
+	api := &service.Service{}
+	api.Initialize()
+
+	http.HandleFunc("/v1/sms/send-confirmation-code", api.SendConfirmationCodeHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
